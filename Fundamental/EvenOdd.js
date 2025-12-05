51c3b07468d77a6ade83594b4ca40370cd1befea
Vinit Kumar Patel <vinitkp9090@gmail.com>
2025-12-05 23:33:23 +0530

    Number is even or odd

---
+++ Fundamental/EvenOdd.js
@@ -0,0 +1,5 @@
+const checkEvenOdd = (num) => num % 2 === 0 ? "Even" : "Odd";
+
+/* function call to check number is even or odd */
+console.log(checkEvenOdd(10)); // Output: Even
+console.log(checkEvenOdd(7));  // Output: Odd
